2eb16f320f23fbecc6ac7fdeb62c8e224146476e
sathish218 <sathishsuganesan@gmail.com>
2025-05-20 23:55:03 +0530

    Add : .env file added

--- supportdesk/src/main/java/com/example/supportdesk/SupportdeskApplication.java
+++ supportdesk/src/main/java/com/example/supportdesk/SupportdeskApplication.java
@@ -2,11 +2,17 @@ package com.example.supportdesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class SupportdeskApplication {
 
+
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(SupportdeskApplication.class, args);
 	}
 
